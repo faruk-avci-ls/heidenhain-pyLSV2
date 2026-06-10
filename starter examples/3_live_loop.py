d06ca9b7ae02f6248abcb6893f745c2c89b466c0
Faruk Avci <avc.faruk10@gmail.com>
2026-06-10 11:39:41 +0300

    Add files via upload

---
+++ starter examples/3_live_loop.py
@@ -0,0 +1,21 @@
+import pyLSV2
+import time
+
+ip = "0.0.0.0" 
+port = 19000
+
+tnc = pyLSV2.LSV2(ip, port, safe_mode = False)
+
+tnc.connect()
+
+while True:
+    axis = tnc.axes_location()
+    override = tnc.override_state()
+    xpos = axis.get('X', 0)
+    ypos = axis.get('Y', 0)
+    zpos = axis.get('Z', 0)
+    print(f"X: {xpos}, Y: {ypos},Z : {zpos}")
+    print(f"Feed: {override.feed}% Rapid: {override.rapid}% Speed: {override.spindle}%")
+    time.sleep(0.2)
+
+
